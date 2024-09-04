3923277e855087eb717b50f93568acec5ac2c35f
hwakk <97441379+hwakk@users.noreply.github.com>
2024-09-04 22:32:48 +0900

    [feat] endpoints 추가

---
+++ src/config/endpoints.js
@@ -0,0 +1,32 @@
+export class EndpointManager {
+  static generateUrl(endpoint, params = {}) {
+    let url = endpoint;
+    for (const key in params) {
+      url = url.replace(`:${key}`, params[key]);
+    }
+    return url;
+  }
+}
+
+export const ENDPOINTS = {
+  HOME: '/',
+  VIDEO_DETAIL: '/contents/:videoId',
+  PEOPLE: '/people/:peopleId',
+  GENRE: '/genres/:genreId',
+  PRODUCTION: '/productions/:productionId',
+  SEARCH: '/search',
+  USER_AUTH_KAKAO_CALLBACK: '/users/auth/kakao/callback',
+  USER_AUTH_NAVER_CALLBACK: '/users/auth/naver/callback',
+  USER_AUTH_GOOGLE_CALLBACK: '/users/auth/google/callback',
+  USER_LOGIN: '/users/login',
+  USER_WATCHTYPE: '/users/watchtype',
+  USER: '/users/:userId',
+  USER_RATINGS: '/users/:userId/contents/ratings',
+  USER_REVIEWS: '/users/:userId/contents/reviews',
+  USER_LIKES: '/users/:userId/contents/likes',
+  USER_PROFILE: '/users/profile',
+  USER_DELETE: '/users/delete',
+  ERROR: '/error',
+  NOT_FOUND: '/404-not-found',
+  DENIED_ALL: '/*',
+};
