fb329fd96315e4fae57feda2b1e4c6ea657103a0
hwakk <97441379+hwakk@users.noreply.github.com>
2024-09-10 03:10:21 +0900

    [fix] user/auth 컴포넌트 use client 명시

--- src/app/users/auth/naver/callback/page.js
+++ src/app/users/auth/naver/callback/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useState } from 'react';
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 // import { AxiosClient } from "@/utils/HttpClient";
