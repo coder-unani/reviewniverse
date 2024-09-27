3c98978fea32c1dd5dae635e8a91bea0e7fd4155
hwakk <97441379+hwakk@users.noreply.github.com>
2024-09-28 02:02:52 +0900

    [test] 구글 로그인 signInWithRedirect 무한루프 오류 테스트

--- src/components/ui/AuthGoogleCallback.jsx
+++ src/components/ui/AuthGoogleCallback.jsx
@@ -32,11 +32,12 @@ const AuthGoogleCallback = () => {
     // 로그인 상태일 경우, 회원 정보 페이지로 이동
     if (user) {
       showErrorToast(MESSAGES['L002']);
-      const path = EndpointManager.generateUrl(ENDPOINTS.USER, { userId: user.id });
-      router.push(path);
+      // const path = EndpointManager.generateUrl(ENDPOINTS.USER, { userId: user.id });
+      // router.push(path);
+      router.push(ENDPOINTS.HOME);
+    } else {
+      handleGoogleLogin();
     }
-
-    handleGoogleLogin();
   }, []);
 
   useEffect(() => {
